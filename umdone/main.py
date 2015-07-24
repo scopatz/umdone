4007c9fb2ecbd32b7f8a9801b1f1163fb47af2e7
Anthony Scopatz <scopatz@gmail.com>
2015-07-23 22:33:07 -0500

    more cli

--- umdone/main.py
+++ umdone/main.py
@@ -1,4 +1,5 @@
 """Main functionality for umdone."""
+import os
 from argparse import ArgumentParser
 
 import librosa
@@ -7,11 +8,12 @@ from umdone import segment
 from umdone import discovery
 
 
-def remove_umms():
-    x, sr = librosa.load(f, sr=None)
-    bounds = segment.boundaries(x, sr)
-    td = discovery.load_training_data(files)
-    matches = discovery.match(x, bounds, td)
+def remove_umms(ns):
+    x, sr = librosa.load(ns.input, sr=None)
+    bounds = segment.boundaries(x, sr, window_length=ns.window_length, 
+                                threshold=ns.noise_threshold)
+    td = discovery.load_training_data(ns.train, n=ns.n_mfcc)
+    matches = discovery.match(x, bounds, td, n=ns.n_mfcc, threshold=ns.match_threashold)
     y = segment.remove_slices(x, matches)
     librosa.output.write_wav('out.wav', y, sr)
     
@@ -19,8 +21,23 @@ def remove_umms():
 def main(args=None):
     """Main umdone entry point."""
     parser = ArgumentParser('umdone')
+    parser.add_argument('input', help='input file')
+    parser.add_argument('-o', '--output', dest='output', default=None, 
+                        help='Output file.')
+    parser.add_argument('-t', '--train', nargs='*', dest='train', 
+                        help='list of training files')
+    parser.add_argument('--window-length', dest='window_length', deafult=0.05,
+                        help='Word boundary window length.')
+    parser.add_argument('--noise-threshold', dest='noise_threshold', default=0.01
+                        'Noise threshold on words vs quiet.')
+    parser.add_argument('--n-mfcc', dest='n_mfcc', deafult=13,
+                        help='Number of MFCC components.')
+    parser.add_argument('--match-threshold', dest='noise_threshold', default=0.45
+                        'Threshold distance to match words.')
     ns = parser.parse_args(args)
-
+    if ns.output is None:
+        ns.output = '{0}-umdone{1}'.format(*os.path.splitext(ns.input))
+    remove_umms(ns)
 
 if __name__ == '__main__':
     main()
